55415941c7db794833412a370df2fe51358cf16a
Pedrostay <pedrostaay@hotmail.com>
2023-09-13 14:36:16 -0300

    att em plans

--- src/components/Plans/index.jsx
+++ src/components/Plans/index.jsx
@@ -1,20 +1,21 @@
 import { Container, Content, Item } from "./style";
 
-import data from "../modules/plans.json"
+import data from "../modules/plans.json";
 export default function PlansSection() {
   return (
     <Container>
-        <h1>Planos</h1>
-        <Content>
-            {data.map((item) => (
-                <Item key={item.id}>
-                    <h2>{item.title}</h2>
-                    <p>{item.description}</p>
-                    <span>R$ {item.price}</span>
-                    <button>Assinar</button>
-                </Item>
-            ))}
-        </Content>
+      <h1>Planos</h1>
+      <Content>
+        {data.map((item) => (
+          <Item key={item.id}>
+            <h2>{item.title}</h2>
+            <h4>{item.category}</h4>
+            <p>{item.description}</p>
+            <span>R$ {item.price}</span>
+            <button>Assinar</button>
+          </Item>
+        ))}
+      </Content>
     </Container>
-  )
+  );
 }
--- src/components/Plans/style.js
+++ src/components/Plans/style.js
@@ -4,20 +4,20 @@ export const Container = styled.section`
   width: 100%;
   padding: 7rem 2rem;
   margin: 5rem 0rem;
-  h1{
+  h1 {
     text-align: center;
     font-size: 4rem;
     color: #e4e4e4;
   }
-`
+`;
 export const Content = styled.div`
   max-width: 1330px;
   margin: 2rem auto;
   display: flex;
-  align-items:center;
+  align-items: center;
   gap: 3rem;
   justify-content: center;
-`
+`;
 export const Item = styled.div`
   text-align: center;
   width: 300px;
@@ -27,31 +27,34 @@ export const Item = styled.div`
   flex-direction: column;
   font-size: 2rem;
   justify-content: center;
-  align-items:center;
+  align-items: center;
   text-align: center;
   background: linear-gradient(120deg, #e46161, #090909);
   color: #e4e4e4;
   animation: left 700ms linear;
   transition: 300ms all linear;
-  font-family:'Montserrat', sans-serif;
-  &:hover{
+  font-family: "Montserrat", sans-serif;
+  &:hover {
     transform: scale(1.1);
   }
   @keyframes left {
-    from{
-      transform: translateY(60px);
+    from {
+      transform: translateY(100px);
     }
-    to{
+    to {
       transform: translateX(0px);
     }
   }
-  p{
+  p {
     font-size: 1.2rem;
-    margin:2rem 2rem;
-    }
+    margin: 2rem 2rem;
+  }
+  h4 {
+    font-size: 1.3rem;
+  }
 
-     button{
-        padding: 12px 50px;
+  button {
+    padding: 12px 50px;
     border-radius: 6px;
     color: #090909;
     font-weight: 500;
@@ -63,5 +66,8 @@ export const Item = styled.div`
     cursor: pointer;
     background-color: #e1e1e1;
     transition: 300ms all linear;
-     }
-`
+  }
+  & {
+    box-shadow: 8px 8px 8px #111;
+  }
+`;
